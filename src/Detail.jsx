cec6cb55a849a2a3307f0fe5edf86e17791f3ca5
mesouissi <souissiimeriem@gmail.com>
2023-06-25 21:27:21 +0200

    test

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Title } from "@ui5/webcomponents-react";
 
 export function Detail() {
-  return <Title>Hello World!</Title>;
+  return <Title>Hello World! this is me !</Title>;
 }
